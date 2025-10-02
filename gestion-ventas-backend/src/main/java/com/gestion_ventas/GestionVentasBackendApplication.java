3e10434bbd374ec8047b451647f57b8cdaa2cf67
Kevin-Mart <xdkevinalexisxd120@gmail-com>
2025-10-01 23:16:11 -0600

    Se añadio la estuctura inicial del backend Spring Boot

---
+++ gestion-ventas-backend/src/main/java/com/gestion_ventas/GestionVentasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVentasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVentasBackendApplication.class, args);
+	}
+
+}
